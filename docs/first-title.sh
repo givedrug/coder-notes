a12c20f62ec1a56256f2747833137544e1153ea1
givedrug <givedrug@outlook.com>
2023-05-20 20:48:50 +0800

    chore：docsify美化

--- docs/first-title.sh
+++ docs/first-title.sh
@@ -2,7 +2,7 @@
 doc_path=docs_copy/
 
 function deal_file(){
-    for element in `ls -1 $1 | sed "s/[ ]/\\\ /g"`
+    for element in `ls -1 $1 | sed "s/[ ]/%20/g"`
     do
         dir_or_file=$1"/"$element
         if [ -d $dir_or_file ] ;
@@ -13,7 +13,7 @@ function deal_file(){
         else
             if [[ "$element" == *"md" ]]; then
               # md文档开头位置增加一个标题，这样docsify显示内部标题的时候就不会忽略原本的第一个标题了
-              sed -i '1i\#\n' $dir_or_file
+              sed -i '1i\#\n' `echo $dir_or_file | sed 's/%20/\ /g'`
             fi
         fi
     done
