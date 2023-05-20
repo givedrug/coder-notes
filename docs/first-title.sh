5629dcfd1f8d2c21088eacef53ad72ee315c6ecb
givedrug <givedrug@outlook.com>
2023-05-20 20:58:51 +0800

    chore：docsify美化

--- docs/first-title.sh
+++ docs/first-title.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 doc_path=docs_copy/
 
 function deal_file(){
   # 将空格替换为%20（html空格）
-    for element in `ls -1 $1 | sed "s/[ ]/%20/g"`
+    for element in `ls -1 $1`
     do
         dir_or_file=$1"/"$element
         if [ -d $dir_or_file ] ;
@@ -15,7 +18,7 @@ function deal_file(){
             if [[ "$element" == *"md" ]]; then
               # md文档开头位置增加一个标题，这样docsify显示内部标题的时候就不会忽略原本的第一个标题了
               # 将%20替换回空格
-              sed -i '1i\#\n' `echo $dir_or_file | sed 's/%20/\\\ /g'`
+              sed -i '1i\#\n' $dir_or_file
             fi
         fi
     done
@@ -34,3 +37,5 @@ do
         deal_file `echo $dir | sed 's/.$//'`
     fi
 done
+
+IFS=$SAVEIFS
