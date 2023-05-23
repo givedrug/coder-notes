6b386e7ec43f2daa700b049e4f0ee5364c0df1ab
givedrug <givedrug@outlook.com>
2023-05-23 19:27:57 +0800

    chore：脚本引擎优化计数逻辑

---
+++ docs/docsify.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os
+
+base_dir = './docs_copy/'
+base_space = 0
+uncheck_dir_list = ['.idea','src']
+
+def changeFile(fullname, name):
+    fr = open(fullname,'r')
+    lines = fr.readlines()
+    fr.close()
+
+    for idx, line in enumerate(lines):
+        lines[idx] = line
+
+    lines.insert(0, '# ' + name.rstrip('.md') + '\n')
+
+    fw = open(fullname,'w')
+    fw.writelines(lines)
+    fw.close()
+
+def checkAllFile(base, file, base_space):
+    list = os.listdir(base)
+    list.sort()
+    for name in list:
+        fullname = os.path.join(base, name)
+        if 'assets' in name:
+            continue
+        if os.path.isdir(fullname) and name not in uncheck_dir_list:
+            file.write(' ' * base_space + '- <font color="#42b983"><b>' + name + '</b></font>' + '\n')
+            checkAllFile(fullname, file, base_space + 2)
+        if os.path.isfile(fullname) and base != base_dir and name.endswith('.md'):
+            file.write(' ' * base_space + '- [' + name.rstrip('.md') + '](' + fullname.lstrip(base_dir) + ')' + '\n')
+            changeFile(fullname, name)
+
+def main():
+    sidebar_file = open(base_dir + '_sidebar.md','w')
+    sidebar_file.write('- [<b>README</b>](README.md)' + '\n')
+    checkAllFile(base_dir, sidebar_file, base_space)
+    sidebar_file.close()
+
+if __name__ == '__main__':
+    main()
