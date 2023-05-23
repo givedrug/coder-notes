b343c6b57ab69d2e0ad5cf3a3131c872eb904204
givedrug <givedrug@outlook.com>
2023-05-23 21:30:46 +0800

    chore：替换为python

--- docs/docsify.py
+++ docs/docsify.py
@@ -5,10 +5,11 @@ import os
 
 base_dir = './docs_copy/'
 base_space = 0
-uncheck_dir_list = ['.idea','src']
+uncheck_dir_list = ['.idea', 'src']
+
 
 def changeFile(fullname, name):
-    fr = open(fullname,'r')
+    fr = open(fullname, 'r')
     lines = fr.readlines()
     fr.close()
 
@@ -17,9 +18,10 @@ def changeFile(fullname, name):
 
     lines.insert(0, '# ' + name.rstrip('.md') + '\n')
 
-    fw = open(fullname,'w')
+    fw = open(fullname, 'w')
     fw.writelines(lines)
     fw.close()
+
 
 def checkAllFile(base, file, base_space):
     list = os.listdir(base)
@@ -32,14 +34,17 @@ def checkAllFile(base, file, base_space):
             file.write(' ' * base_space + '- <font color="#42b983"><b>' + name + '</b></font>' + '\n')
             checkAllFile(fullname, file, base_space + 2)
         if os.path.isfile(fullname) and base != base_dir and name.endswith('.md'):
-            file.write(' ' * base_space + '- [' + name.rstrip('.md') + '](' + fullname.lstrip(base_dir) + ')' + '\n')
+            file.write(' ' * base_space + '- [' + name.rstrip('.md') + '](' + fullname.lstrip(base_dir).replace(' ',
+                                                                                                                '%20') + ')' + '\n')
             changeFile(fullname, name)
 
+
 def main():
-    sidebar_file = open(base_dir + '_sidebar.md','w')
+    sidebar_file = open(base_dir + '_sidebar.md', 'w')
     sidebar_file.write('- [<b>README</b>](README.md)' + '\n')
     checkAllFile(base_dir, sidebar_file, base_space)
     sidebar_file.close()
+
 
 if __name__ == '__main__':
     main()
